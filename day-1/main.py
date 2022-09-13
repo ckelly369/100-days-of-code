cb58e5dd767bb77d18098f6eff629fe4a644ce67
connor.kelly <connor.kelly@and.digital>
2022-09-13 11:49:28 +0100

    Adding day 1 session

---
+++ day-1/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator\nWhat's the name of the City you grew up in?\n"" ")
+city = input("City: ")
+
+print("\nWhat's the name of your first pet?\n")
+pet = input("Pet name: ")
+
+print("\nYour new band name could be " + city + " " + pet)
