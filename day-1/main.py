bd43ce06e8692d297da6f83a8fc3e78bef61fee1
connor.kelly <connor.kelly@and.digital>
2022-09-14 13:43:20 +0100

    .

--- day-1/main.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the Band Name Generator\nWhat's the name of the City you grew up in?\n"" ")
-city = input("City: ")
-
-print("\nWhat's the name of your first pet?\n")
-pet = input("Pet name: ")
-
-print("\nYour new band name could be " + city + " " + pet)
-print(city[1])
