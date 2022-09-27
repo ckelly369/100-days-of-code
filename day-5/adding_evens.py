593bf96aa15cdefab8f7a5f9c209a3efe6f501b8
connor.kelly <connor.kelly@and.digital>
2022-09-27 16:51:52 +0100

    day-5 added

---
+++ day-5/adding_evens.py
@@ -0,0 +1,6 @@
+number = 0
+
+for i in range(0, 101, 2):
+    number += i
+
+print(number)
---
+++ day-5/password_generator.py
@@ -0,0 +1,53 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# Number of letters to be inputted by the user
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# The below will order by letters, symbols and numbers.
+# password = ""
+
+# for letter in range(1, nr_letters +1):
+#     random_letter = random.choice(letters)
+#     password += random_letter
+
+# for symbol in range(1, nr_symbols +1):
+#     random_symbol = random.choice(symbols)
+#     password += random_symbol
+
+# for number in range(1, nr_numbers +1):
+#     random_number = random.choice(numbers)
+#     password += random_number
+
+# print(password)
+
+# To randomise every character, we can put the password as a list, rather than an empty string.
+# The below order will be completely random. This is done by using the shuffle method
+# and then turning the password_list into a string.
+
+password_list = []
+
+for letter in range(1, nr_letters +1):
+    random_letter = random.choice(letters)
+    password_list += random_letter
+
+for symbol in range(1, nr_symbols +1):
+    random_symbol = random.choice(symbols)
+    password_list += random_symbol
+
+for number in range(1, nr_numbers +1):
+    random_number = random.choice(numbers)
+    password_list += random_number
+
+random.shuffle(password_list)
+
+password = ""
+
+for char in password_list:
+    password += char
+print(f"Your password is: {password}")
