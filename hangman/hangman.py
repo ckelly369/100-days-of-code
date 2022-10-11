f4fa220d1c2aa60860b16952438b26af94bfd423
connor.kelly <connor.kelly@and.digital>
2022-10-11 10:35:14 +0100

    adding new stages

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,9 +1,12 @@
 import random
+import hangman_art
+from word_list import words
 
 # Three words in a list.
 # A random word is defined in a new variable.
 # Printed the chosen word.
-word_list = ["ardvark", "baboon", "camel"]
+print(hangman_art.logo)
+word_list = words
 chosen_word = random.choice(word_list)
 print(f"The word that was chosen is {chosen_word}.")
 
@@ -17,18 +20,33 @@ for blank in chosen_word:
 
 print(display)
 
-# User input.
-guess = input("Guess a letter: ").lower()
+end_of_game = False
+lives = 6
 
-# The range is between 0 and the word length (length of chosen word).
-# On the first run, the position of chosen_word is index 0.
-# So the letter variable becomes equal to the first letter of the chosen word.
-# Statement to check if the letter matches the guess.
-# If False, go back to the for loop and move to next index position.
-# If True, look at the list (display) and replace the blank with the matching letter.
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-print(display)
+    # The range is between 0 and the word length (length of chosen word).
+    # On the first run, the position of chosen_word is index 0.
+    # So the letter variable becomes equal to the first letter of the chosen word.
+    # Statement to check if the letter matches the guess.
+    # If False, go back to the for loop and move to next index position.
+    # If True, look at the list (display) and replace the blank with the matching letter.
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You now have {lives} lives.")
+        if lives == 0:
+            end_of_game = True
+            print("Game over.")
+    print(display) 
+
+    if "_" not in display:
+        end_of_game = True
+        print("You have won!")
+
+print(hangman_art.stages[lives])
